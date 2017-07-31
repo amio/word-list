63d18e83f8efd8e7848af366476502c56805950b
Amio <amio.cn@gmail.com>
2017-07-31 15:51:28 +0800

    Use standard instead of xo.

--- fetch-words.js
+++ fetch-words.js
@@ -1,12 +1,12 @@
-'use strict';
-const fs = require('fs');
-const got = require('got');
+'use strict'
+const fs = require('fs')
+const got = require('got')
 
-const url = 'https://raw.github.com/atebits/Words/master/Words/en.txt';
+const url = 'https://raw.github.com/atebits/Words/master/Words/en.txt'
 
 got(url).then(res => {
-	fs.writeFileSync('words.txt', res.body.trim());
+  fs.writeFileSync('words.txt', res.body.trim())
 }).catch(err => {
-	console.error(err);
-	process.exit(1); // eslint-disable-line unicorn/no-process-exit
-});
+  console.error(err)
+  process.exit(1) // eslint-disable-line unicorn/no-process-exit
+})
--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-import fs from 'fs'
 import test from 'ava'
 import m from './'
 
 test(t => {
-  t.true(m.length > 0)
-  t.true(fs.statSync(m).size > 1000)
+  t.true(typeof m === 'string')
+  t.true(m.length > 1000)
 })
